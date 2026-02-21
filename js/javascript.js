3df9c5d70358297c61b3d0d02ffea85338082173
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-02-21 10:27:11 +0630

    Create 3x3 2d array gameboard with value 0s

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,28 @@
-console.log('Ok');
+function gameBoard() {
+    const row = 3;
+    const column = 3;
+    const board = [];
+
+    for(let i = 0; i < row; i++) {
+        board[i] = [];
+        for(let j = 0; j < column; j++) {
+            board[i].push(cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    function cell() {
+        let value = 0;
+
+        const getValue = () => value;
+
+        return {
+            getValue,
+        }
+    }
+
+    return {
+        getBoard,
+    }
+}
