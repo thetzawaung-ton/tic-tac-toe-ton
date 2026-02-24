659a0acb178ed554a6d3523f270425b975d44f23
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-02-24 10:22:44 +0630

    Add tie logic

--- js/javascript.js
+++ js/javascript.js
@@ -3,13 +3,16 @@ function gameBoard() {
     const column = 3;
     const board = [];
 
-    for(let i = 0; i < row; i++) {
-        board[i] = [];
-        for(let j = 0; j < column; j++) {
-            board[i].push(cell());
+    const setInitialBoard = () => {
+
+        for(let i = 0; i < row; i++) {
+            board[i] = [];
+            for(let j = 0; j < column; j++) {
+                board[i].push(cell());
+            }
         }
     }
-
+    setInitialBoard();
     const getBoard = () => board;
 
     const printBoard = () => {
@@ -51,6 +54,7 @@ function gameBoard() {
         printBoard,
         dropToken,
         checkCellValue,
+        setInitialBoard,
     }
 }
 
@@ -61,11 +65,11 @@ function gameController(
     const players = [
         {
             name: playerOneName,
-            token: 1,
+            token: "O",
         },
         {
             name: playerTwoName,
-            token: 2,
+            token: "X",
         }
     ];
     const board = gameBoard();
@@ -75,11 +79,12 @@ function gameController(
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
     const getActivePlayer = () => activePlayer;
-    
+    let moveCount = 0;
 
     const playRound = (row, column) => {
         console.log(`${getActivePlayer().name} dropping token`);
         const playerToken = getActivePlayer().token;
+        moveCount += 1;
 
         if(board.dropToken(row, column, playerToken)) {
             if(board.checkCellValue(0, 0) === playerToken && board.checkCellValue(0, 1) === playerToken && board.checkCellValue(0, 2) === playerToken ||
@@ -91,6 +96,14 @@ function gameController(
         board.checkCellValue(0, 0) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(2, 2) === playerToken ||
         board.checkCellValue(0, 2) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(2, 0) === playerToken) {          
             console.log(`${getActivePlayer().name} is the winner`);
+            board.setInitialBoard();
+            activePlayer = players[0];
+            return
+        }
+        if(moveCount == 9) {
+            console.log("It is a tie");
+            board.setInitialBoard();
+            activePlayer = players[0];
             return
         }
             switchPlayerTurn();
