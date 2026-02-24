079990bb478f10e72105ebec464135fea0dbe0e7
thetzawaung-ton <241034270+thetzawaung-ton@users.noreply.github.com>
2026-02-24 09:36:29 +0630

    Add winning logic

--- js/javascript.js
+++ js/javascript.js
@@ -44,10 +44,13 @@ function gameBoard() {
         }
     }
 
+    const checkCellValue = (row, column) => board[row][column].getValue();
+
     return {
         getBoard,
         printBoard,
         dropToken,
+        checkCellValue,
     }
 }
 
@@ -72,11 +75,24 @@ function gameController(
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
     const getActivePlayer = () => activePlayer;
+    
 
     const playRound = (row, column) => {
         console.log(`${getActivePlayer().name} dropping token`);
+        const playerToken = getActivePlayer().token;
 
-        if(board.dropToken(row, column, getActivePlayer().token)) {
+        if(board.dropToken(row, column, playerToken)) {
+            if(board.checkCellValue(0, 0) === playerToken && board.checkCellValue(0, 1) === playerToken && board.checkCellValue(0, 2) === playerToken ||
+        board.checkCellValue(1, 0) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(1, 2) === playerToken ||
+        board.checkCellValue(2, 0) === playerToken && board.checkCellValue(2, 1) === playerToken && board.checkCellValue(2, 2) === playerToken ||
+        board.checkCellValue(0, 0) === playerToken && board.checkCellValue(1, 0) === playerToken && board.checkCellValue(2, 0) === playerToken ||
+        board.checkCellValue(0, 1) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(2, 1) === playerToken ||
+        board.checkCellValue(0, 2) === playerToken && board.checkCellValue(1, 2) === playerToken && board.checkCellValue(2, 2) === playerToken ||
+        board.checkCellValue(0, 0) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(2, 2) === playerToken ||
+        board.checkCellValue(0, 2) === playerToken && board.checkCellValue(1, 1) === playerToken && board.checkCellValue(2, 0) === playerToken) {          
+            console.log(`${getActivePlayer().name} is the winner`);
+            return
+        }
             switchPlayerTurn();
         }
         console.log(`${activePlayer.name}'s turn`);
